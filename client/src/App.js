42c4830402f5fb3677eb0593e42c47e7a5ebc3aa
Nir Adler <niradler55@gmail.com>
2018-06-16 18:36:36 +0300

    client init

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,123 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import server from './server'
 import logo from './logo.svg';
 import './App.css';
+import Table from "./components/Table";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      tableData: false,
+      aggregatedTableData: false,
+      supportedCurrencies: false,
+      loading: true
+    }
+  }
+
+  async componentDidMount() {
+    await this.init()
+  }
+
+  getUpdatedData = async() => {
+    try {
+      await this.setState({loading: true});
+      const tableData = await server.getTableData();
+      const aggregatedTableData = await server.getAggregatedTableData();
+      this.setState({tableData: tableData.data, aggregatedTableData: aggregatedTableData.data, loading: false});
+    } catch (error) {
+      console.log('getUpdatedData', error);
+    }
+  }
+
+  init = async() => {
+    try {
+      const supportedCurrencies = await server.getSupportedCurrencies();
+      await this.getUpdatedData();
+      this.setState({supportedCurrencies: supportedCurrencies.data});
+    } catch (error) {
+      console.log('init', error);
+    }
+
+    const interval = setInterval(() => {
+      this
+        .getUpdatedData()
+        .then((done) => {})
+    }, 10000);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="hero is-primary is-medium">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                <i className="fas fa-chart-area"></i>
+                Foreign Exchange
+              </h1>
+              <h2 className="subtitle">
+                Foreign exchange demo tool
+              </h2>
+            </div>
+          </div>
+        </section>
+        <div className="container">
+          <div className="columns">
+            <nav className="panel column is-7">
+              <p className="panel-heading">
+                Financial unit and their value in USD {this.state.loading
+                  ? <span className="is-pulled-right">
+                      <i className="fas fa-spinner fa-pulse"></i>
+                    </span>
+                  : ''}
+              </p>
+              <div className="panel-block">
+                {this.state.tableData
+                  ? <Table data={this.state.tableData}/>
+                  : <div className="spinner-container">
+                    <i className="fas fa-spinner fa-pulse"></i>
+                  </div>}
+              </div>
+            </nav>
+            <div className="column is-5">
+              <div className="columns">
+                <nav className="panel column is-12">
+                  <p className="panel-heading">
+                    Financial unit and their total value in USD {this.state.loading
+                      ? <span className="is-pulled-right">
+                          <i className="fas fa-spinner fa-pulse fa-lg"></i>
+                        </span>
+                      : ''}
+                  </p>
+                  <div className="panel-block">
+                    {this.state.aggregatedTableData
+                      ? <Table data={this.state.aggregatedTableData}/>
+                      : <div className="spinner-container">
+                        <i className="fas fa-spinner fa-pulse fa-lg"></i>
+                      </div>}
+                  </div>
+                </nav>
+              </div>
+              <div className="columns">
+                <div className="column is-12">
+                  <CurrencyConverter/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="container">
+            <div className="content has-text-centered">
+              <p>
+                <strong>Foreign exchange demo</strong>
+                by Nir Adler
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
